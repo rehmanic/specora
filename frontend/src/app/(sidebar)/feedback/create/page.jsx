8c223b947380c45abe6d309ff1afa113f2240177
Rehman-codes <abdurrehman.swe@gmail.com>
2025-10-11 03:45:16 +0500

    Added Form builder editor using Survey JS

--- frontend/src/app/(sidebar)/feedback/create/page.jsx
+++ frontend/src/app/(sidebar)/feedback/create/page.jsx
@@ -1,10 +1,11 @@
-import FeedbackForm from "@/components/feedback/FeedbackForm";
+"use client";
+
+import FormBuilder from "@/components/feedback/FormBuilder";
 
 export default function CreateFeedbackPage() {
   return (
-    <section className="rounded-lg border border-border bg-card p-4 md:p-6">
-      <h2 className="text-lg font-medium mb-4">Write Feedback</h2>
-      <FeedbackForm redirectTo="/feedback" />
+    <section className="border w-full flex justify-center items-center">
+      <FormBuilder />
     </section>
   );
 }
--- frontend/src/components/feedback/UseFeedback.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-export function useFeedback() {
-  const [data, setData] = useState([]);
-  const add = (feedback) => setData((prev) => [...prev, feedback]);
-  return { data, add };
-}
-
